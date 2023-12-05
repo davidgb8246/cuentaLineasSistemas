57ce083e197c892664a9a313e89d54b59fa7477b
davidgb8246 <85250523+davidgb8246@users.noreply.github.com>
2023-12-05 17:15:04 +0100

    script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "¡¡¡ ATENCIÓN !!!" 
+echo "A la hora de poner la ruta de la carpeta, tendrás que especificar siempre la "/" del final."
+
 echo Dime el nombre del archivo que contendrá las lineas invertidas por cada alumno
 read nombreTabla
 
